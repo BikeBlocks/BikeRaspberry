61b9a50382fcd5854643c168bd2834f55c5d18d1
mininao <maxence_aici@yahoo.fr>
2015-06-11 23:12:06 +0200

    Socket.io hello world

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+var bike = {
+	dummy: true
+};
+
+if(bike.dummy) {
+	var gyro = require("./lib/dummyGyro.js");
+	var speed = require("./lib/dummySpeed.js");
+	var lights = require("./lib/dummyLights.js");
+} else {
+	var gyro = require("./lib/gyro.js");
+	var speed = require("./lib/speed.js");
+	var lights = require("./lib/lights.js");
+}
+
+
+
+/* Express http Server */
 var express = require('express');
 var app = express();
 
@@ -10,4 +27,16 @@ var server = app.listen(3000, function () {
 
   console.log('Test app online at http://%s:%s', host, port);
 
+});
+
+/* Socket Communication */
+var io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+  socket.emit('update', { hello: 'world' });
+	
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+	
 });
